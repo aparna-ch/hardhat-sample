1aa059c709929a3ca4c008381dccf5ce5d235a0a
aparna-ch <aparnahariekm10@gmail.com>
2022-08-06 00:20:52 +0530

    msg

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
 require("dotenv").config();
 
+
 module.exports = {
   solidity: "0.8.4",
   networks: {
@@ -9,10 +10,9 @@ module.exports = {
       chainId: 1337,
     },
     mumbai: {
-      url: "https://polygon-mumbai.infura.io/v3/5e7a16f906534263988e6ca0bc8547d8",
-      accounts: ["0x652b8fff30b27c61e02434e0b77e12da63949d7724671a79fb85a4097347976e"],
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
       gas: 2100000,
       gasPrice: 8000000000,
     },
-  },
-}
+  }}
